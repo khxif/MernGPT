b6f07d3539363033026a4d00f0635c1d0e76d63f
Khaif Mohamed <khaifmohamededava@gmail.com>
2024-01-12 13:12:29 +0530

    removed dist folder

--- server/dist/routes/ChatRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ChatController_1 = require("../controllers/ChatController");
-const router = (0, express_1.Router)();
-router.post("/chat-completion", ChatController_1.verifyUser, ChatController_1.createChatCompletion);
-router.get("/get-messages", ChatController_1.verifyUser, ChatController_1.getAllMessages);
-router.get("/clear-chat", ChatController_1.verifyUser, ChatController_1.clearChat);
-exports.default = router;
