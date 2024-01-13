c2f2f16eb3ad700e70cbece7ed44fe8222fac9ce
Khaif Mohamed <khaifmohamededava@gmail.com>
2024-01-13 20:40:31 +0530

    responsive fix

--- client/src/components/chat/ChatBody.tsx
+++ client/src/components/chat/ChatBody.tsx
@@ -17,7 +17,7 @@ export default function ChatBody({ userCookie }: ChatBodyProps) {
   console.log(chats);
   return (
     <>
-      <div className="flex-1 h-full overflow-y-scroll py-2 md:py-6 pb-40 scrollbar-hide">
+      <div className="flex-1 h-full overflow-y-scroll pt-2 md:pt-6 pb-40 scrollbar-hide">
         {chats?.map((chat: any, i: number) => (
           <ChatRow key={i} chat={chat} />
         ))}
